15890fa464bb4595452b2bb4de062efade47aeec
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-12 09:40:22 -0300

    atualizacao 19-jsf (ainda com bugs)

--- 19-JSF/src/CarrinhoBean.java
+++ 19-JSF/src/CarrinhoBean.java
@@ -2,34 +2,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarrinhoBean {
-	private List<ProdutoBean> produto = new ArrayList<ProdutoBean>();	
-	private int quantidade;
-	private double valor;
-	public CarrinhoBean(List<ProdutoBean> produto, int quantidade, double valor) {
-		super();
-		this.produto = produto;
-		this.quantidade = quantidade;
-		this.valor = valor;
-	}
+	private List<ProdutoBean> listaSelecionados = new ArrayList<ProdutoBean>();
+	private String quantidade;
+	private String pagamento;
+
+	public void comprar() {
+		for(ProdutoBean valor : listaSelecionados)
+			System.out.println(valor.getValor());	
+		System.out.println("Quantidade: " + getQuantidade());
+		System.out.println("Forma de pagamento: " + getPagamento());
+	}	
 	public CarrinhoBean() {
 		super();
 	}
-	public List<ProdutoBean> getProduto() {
-		return produto;
+	public List<Forma> getFormasDePagamento(){
+		List<Forma> formas = new ArrayList<Forma>();
+		formas.add(new Forma(1, "Crédito"));
+		formas.add(new Forma(1, "Débito"));
+		formas.add(new Forma(1, "Boleto"));
+		return formas;
 	}
-	public void setProduto(List<ProdutoBean> produto) {
-		this.produto = produto;
-	}
-	public int getQuantidade() {
+	public List<ProdutoBean> getProdutos() {
+		List<ProdutoBean> lista = new ArrayList<ProdutoBean>();
+		lista.add(new ProdutoBean(1, "Notebook", 2500));
+		lista.add(new ProdutoBean(2, "Celular", 2000));
+		lista.add(new ProdutoBean(3, "Teclado", 250));
+		return lista;
+	}	
+	public String getQuantidade() {
 		return quantidade;
 	}
-	public void setQuantidade(int quantidade) {
+	public void setQuantidade(String quantidade) {
 		this.quantidade = quantidade;
 	}
-	public double getValor() {
-		return valor;
+	public String getPagamento() {
+		return pagamento;
 	}
-	public void setValor(double valor) {
-		this.valor = valor;
-	}	
+	public void setPagamento(String pagamento) {
+		this.pagamento = pagamento;
+	}
 }
